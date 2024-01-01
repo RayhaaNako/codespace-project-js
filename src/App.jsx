03a3122475c6db6a8ec73e1eca4384828b8a4309
Rayhaan Imtiyaz Wijanarko <71748270+RayhaaNako@users.noreply.github.com>
2024-01-01 02:23:46 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Rayhaan Imtiyaz",
+  title: "Web & Graphic Designer",
+  email: "rayhaanimtiyaz1301@gmail.com",
+  gitHub: "https://github.com/RayhaaNako",
+  instagram: "https://www.instagram.com/rayhaanako_/",
+  linkedIn: "https://www.linkedin.com/in/rayhaaniw1301/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
